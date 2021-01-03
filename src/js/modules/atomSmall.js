2f522f4500802236f2732ed8b7eb1fa7418778b1
Joseph May <jwmay87@gmail.com>
2021-01-03 15:47:00 -0800

    darken color of small atoms

--- src/js/modules/atomSmall.js
+++ src/js/modules/atomSmall.js
@@ -2,7 +2,7 @@ import { Draggable } from './draggable'
 
 export class AtomSmall extends Draggable {
   constructor(p, x, y, label) {
-    const colors = { main: '#b5df63', drag: '#9bd42d', over: '#c3e582' }
+    const colors = { main: '#9bd42d', drag: '#749f20', over: '#b3de61' }
     super(p, x, y, 30, 30, colors, label)
   }
 }
